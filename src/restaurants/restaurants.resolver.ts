88524ff92f80c92043503cc6d08a006ee2f5268b
qudwnbj <78632744+qudwnbj@users.noreply.github.com>
2021-04-02 15:06:12 +0900

    Edit Restaurant
    nomadcoder lecture #10.10

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,13 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { Role } from 'src/auth/role.decorator';
+import { EditProfileOutput } from 'src/users/dtos/edit-profile.dto';
 import { User } from 'src/users/entities/user.entity';
 import {
   CreateRestaurantInput,
   CreateRestaurantOutput,
 } from './dtos/create-restaurant.dto';
+import { EditRestaurantInput } from './dtos/edit-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -13,7 +15,7 @@ import { RestaurantService } from './restaurants.service';
 export class RestaurantsResolver {
   constructor(private readonly restaurantService: RestaurantService) {}
 
-  @Mutation(returns => Boolean)
+  @Mutation(returns => CreateRestaurantOutput)
   @Role(['Owner'])
   async createrestaurant(
     @AuthUser() authUser: User,
@@ -23,5 +25,14 @@ export class RestaurantsResolver {
       authUser,
       createRestaurantInput,
     );
+  }
+
+  @Mutation(returns => EditProfileOutput)
+  @Role(['Owner'])
+  editRestaurant(
+    @AuthUser() owner: User,
+    @Args('input') editRestaurantInput: EditRestaurantInput,
+  ): Promise<EditProfileOutput> {
+    return this.restaurantService.editRestaurant(owner, editRestaurantInput);
   }
 }
