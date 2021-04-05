326bd21d382acc7717121ddbb61b24906a0259db
qudwnbj <78632744+qudwnbj@users.noreply.github.com>
2021-04-05 15:17:25 +0900

    create order
    nomadcoder lecture #11.12

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -5,11 +5,13 @@ import {
   ObjectType,
   registerEnumType,
 } from '@nestjs/graphql';
+import { IsEnum, IsNumber } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+import { OrderItem } from './order-item.entity';
 
 enum OrderStatus {
   Pending = 'Pending',
@@ -20,7 +22,7 @@ enum OrderStatus {
 
 registerEnumType(OrderStatus, { name: 'OrderStatus' });
 
-@InputType('RestaurantInputType', { isAbstract: true })
+@InputType('OrderInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
 export class Order extends CoreEntity {
@@ -38,23 +40,25 @@ export class Order extends CoreEntity {
   })
   driver?: User;
 
-  @Field(type => Restaurant)
+  @Field(type => Restaurant, { nullable: true })
   @ManyToOne(type => Restaurant, restaurant => restaurant.orders, {
     onDelete: 'SET NULL',
     nullable: true,
   })
-  restaurant: Restaurant;
+  restaurant?: Restaurant;
 
-  @Field(type => [Dish])
-  @ManyToMany(type => Dish)
+  @Field(type => [OrderItem])
+  @ManyToMany(type => OrderItem)
   @JoinTable()
-  dishes: Dish[];
+  items: OrderItem[];
 
-  @Column()
-  @Field(type => Float)
-  total: number;
+  @Column({ nullable: true })
+  @Field(type => Float, { nullable: true })
+  @IsNumber()
+  total?: number;
 
-  @Column({ type: 'enum', enum: OrderStatus })
+  @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.Pending })
   @Field(type => OrderStatus)
+  @IsEnum(OrderStatus)
   status: OrderStatus;
 }
---
+++ src/orders/orders.resolver.ts
@@ -0,0 +1,22 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { User } from 'src/users/entities/user.entity';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { OrderService } from './orders.service';
+
+@Resolver(of => Restaurant)
+export class OrderResolver {
+  constructor(private readonly ordersService: OrderService) {}
+
+  @Mutation(returns => CreateOrderOutput)
+  @Role(['Client'])
+  async createOrder(
+    @AuthUser() customer: User,
+    @Args('input')
+    createOrderInput: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    return this.ordersService.createOrder(customer, createOrderInput);
+  }
+}
